510c200db91510c18ad6d815a2f6f78a58f4e8d5
alex <email>
2021-02-20 08:11:35 +0300

    adedd minus

---
+++ calcthis/minus.py
@@ -0,0 +1,5 @@
+def minus_it(a, b):
+    print(a - b)
+
+if __name__ == '__main__':
+    minus_it(7, 3)
--- calcthis/plus.py
+++ calcthis/plus.py
@@ -5,4 +5,4 @@ def plus_it_double(a, b):
     print((a + b) * 2)
 
 if __name__ == '__main__':
-    plus_it(5, 3)
+    plus_it(4, 3)
